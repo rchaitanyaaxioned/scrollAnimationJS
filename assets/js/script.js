4f0a7c8598e109dfd58dd65504b0d7a5c343ee99
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-07-01 12:28:34 +0530

    JS code written

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
-/* Author: 
+function show() {
+    var cards = document.querySelectorAll(".card");
 
-*/
+    for (var i = 0; i < cards.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = cards[i].getBoundingClientRect().top;
+        var elementVisible = 150;
 
+        if (elementTop < windowHeight - elementVisible) {
+            cards[i].classList.add("active");
+        } else {
+            cards[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", show);
 
 
 
